05976842d7642d02600bdc5c4f3ea9085683a338
Arkadiusz Balys <arkadiusz.balys@nordicsemi.no>
2026-01-07 11:52:07 +0100

    Revert "[nrf fromtree] [spinel] do not update the sent frame if the frame has been updated before sending (#10976)"
    This reverts commit bd1db6192bc361c1b25a7e3d77d51211415caa74.

--- src/core/diags/factory_diags.hpp
+++ src/core/diags/factory_diags.hpp
@@ -261,10 +261,9 @@ private:
     uint8_t       mChannel;
     int8_t        mTxPower;
     uint8_t       mTxLen;
-    bool          mIsHeaderUpdated : 1;
-    bool          mIsTxPacketSet : 1;
-    bool          mRepeatActive : 1;
-    bool          mDiagSendOn : 1;
+    bool          mIsTxPacketSet;
+    bool          mRepeatActive;
+    bool          mDiagSendOn;
 #endif
 
     ReceiveConfig        mReceiveConfig;
